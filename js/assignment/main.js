9a8a340925b50b9f4206cf098bb3257b8210a574
nalinc <nalin.chhibber@gmail.com>
2014-10-21 18:42:53 +0530

    update

--- js/assignment/main.js
+++ js/assignment/main.js
@@ -32,6 +32,9 @@ function vStrength()
 	document.getElementById("pwd_c").style.background="white";
 	document.getElementById("pwd_d").style.background="white";
 	
+	document.getElementById("vpwd").innerHTML="";
+	document.getElementById("rpwd").value="";
+
 	if(isValid(p))
 	{
 
@@ -101,12 +104,78 @@ It may optionally contain:\n\
 
 function validate()
 {
-	var name=document.getElementById("fname").value+document.getElementById("lname").value;
-	if(name.match(/[0-9]|!|@|#|*|_|-/g))
+	var fname=document.getElementById("fname").value;
+	var lname=document.getElementById("lname").value;
+	var name=fname+lname;
+	document.getElementById("vdob").innerHTML="";
+	document.getElementById("vname").innerHTML="";
+	document.getElementById("vgender").innerHTML="";
+	document.getElementById("vlocation").innerHTML="";
+	document.getElementById("vpostal").innerHTML="";
+	document.getElementById("terms").innerHTML="";	
+
+
+	if(name.match(/[0-9|!|@|#|*|_|-]/g) || fname== "" || lname=="")
 	{	document.getElementById("vname").style.color="red";
-		document.getElementById("vname").innerHTML="Invalid";	
+		document.getElementById("vname").innerHTML="Invalid Name";	
 	}
 
+	if(document.getElementById("gender").value == "- Select One -")
+	{	document.getElementById("vgender").style.color="red";
+		document.getElementById("vgender").innerHTML="Please select your gender!";	
+	}
 
+	var d=document.getElementById("day").value;
+	var m=document.getElementById("month").value;
+	var y=document.getElementById("year").value;
+	document.getElementById("vdob").style.color="red";
+
+	if(m == "- Select Month -" || d == "" || y == "")
+		document.getElementById("vdob").innerHTML="Invalid Date!";	
 	
+
+	if((y%4==0 && y%100==0 )|| y%400==0)
+	{	if(m=="Febuary" && d>29 || d<1)
+			document.getElementById("vdob").innerHTML="Invalid Date!";
+	}else
+	{
+		if(m=="Febuary" && d>28 || d<1)
+			document.getElementById("vdob").innerHTML="Invalid Date!";
+	}
+
+	var mth=["January","March","May","July","August","October","December"]
+	if(mth.indexOf(m) !=-1 && d>31 || d<1)
+			document.getElementById("vdob").innerHTML="Invalid Date!";
+
+	var mth=["April","June","September","November"];
+	if(mth.indexOf(m) !=-1 && d>30 || d<1)
+			document.getElementById("vdob").innerHTML="Invalid Date!";
+
+	if(document.getElementById("location").value == "- Select Country -")
+	{	document.getElementById("vlocation").style.color="red";
+		document.getElementById("vlocation").innerHTML="Please select your country!";	
+	}
+
+	var pc=document.getElementById("postal").value;
+	if(isNaN(pc)||pc=="")
+	{	document.getElementById("vpostal").style.color="red";
+		document.getElementById("vpostal").innerHTML="Invalid!";		
+	}
+
+	vpwd();
+
+ /*   if (document.getElementById('terms').checked) 
+    {
+		document.getElementById("terms").innerHTML="";
+     } else {
+		document.getElementById("terms").innerHTML="You must agree to our Terms of Service.!";	
+        }
+
+	*/
+
+	 if (document.getElementById('terms').checked) {
+            alert("checked");
+        } else {
+            alert("You didn't check it! Let me check it for you.");
+        } 
 }
